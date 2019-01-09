fc6023278d6a8a33812e18e373d478f12102e9ff
Helena A. Bales <hbales@pivotal.io>
2019-01-09 10:36:29 -0800

    add memory tuning parameters to reduce GC

--- geode-benchmarks/src/main/java/org/apache/geode/benchmark/parameters/JVMParameters.java
+++ geode-benchmarks/src/main/java/org/apache/geode/benchmark/parameters/JVMParameters.java
@@ -40,8 +40,10 @@ public class JVMParameters {
       "-Dgemfire.OSProcess.ENABLE_OUTPUT_REDIRECTION=true",
       "-Dgemfire.launcher.registerSignalHandlers=true",
       "-Xmx8g",
-      "-Xms8g"
-
+      "-Xms8g",
+      "-XX:+PrintTenuringDistribution",
+      "-XX:SurvivorRatio=1",
+      "-XX:TargetSurvivorRatio=80"
   };
 
 }
