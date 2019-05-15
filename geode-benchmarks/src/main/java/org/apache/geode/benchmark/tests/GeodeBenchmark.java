47b41d96bf52d6fd953383fade5b66f11feecb08
Jacob Barrett <jbarrett@pivotal.io>
2019-05-15 08:51:40 -0700

    16 threads

--- geode-benchmarks/src/main/java/org/apache/geode/benchmark/tests/GeodeBenchmark.java
+++ geode-benchmarks/src/main/java/org/apache/geode/benchmark/tests/GeodeBenchmark.java
@@ -34,7 +34,7 @@ public class GeodeBenchmark {
   /**
    * Number of threads to run benchmark.
    */
-  private static final int THREADS = 32;
+  private static final int THREADS = 16;
 
 
   public static TestConfig createConfig() {
