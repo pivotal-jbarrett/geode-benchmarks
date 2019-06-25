6d8773a88680d2c06b6aa97c709dd6e99524b14d
Jacob Barrett <jbarrett@pivotal.io>
2019-06-25 12:39:07 -0700

    Put Long benchmark

---
+++ geode-benchmarks/src/main/java/org/apache/geode/benchmark/tasks/PutLongTask.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.geode.benchmark.tasks;
+
+import java.io.Serializable;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+
+import benchmark.geode.data.Portfolio;
+import org.yardstickframework.BenchmarkConfiguration;
+import org.yardstickframework.BenchmarkDriverAdapter;
+
+import org.apache.geode.cache.Region;
+import org.apache.geode.cache.client.ClientCache;
+import org.apache.geode.cache.client.ClientCacheFactory;
+
+public class PutLongTask extends BenchmarkDriverAdapter implements Serializable {
+
+  private Region<Object, Object> region;
+
+  final private long keyRange;
+
+  public PutLongTask(final long keyRange) {
+    this.keyRange = keyRange;
+  }
+
+  @Override
+  public void setUp(final BenchmarkConfiguration cfg) throws Exception {
+    super.setUp(cfg);
+    final ClientCache cache = ClientCacheFactory.getAnyInstance();
+    region = cache.getRegion("region");
+  }
+
+  @Override
+  public boolean test(final Map<Object, Object> ctx) {
+    final Long key = ThreadLocalRandom.current().nextLong(0, this.keyRange);
+    region.put(key, key);
+    return true;
+  }
+}
