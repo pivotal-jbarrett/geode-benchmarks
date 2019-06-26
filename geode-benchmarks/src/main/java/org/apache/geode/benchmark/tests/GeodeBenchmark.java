f354f7267683e11045140ca5cb5c8a580c37274a
Jacob Barrett <jbarrett@pivotal.io>
2019-06-25 19:07:15 -0700

    Shorter runtime

--- geode-benchmarks/src/main/java/org/apache/geode/benchmark/tests/GeodeBenchmark.java
+++ geode-benchmarks/src/main/java/org/apache/geode/benchmark/tests/GeodeBenchmark.java
@@ -16,6 +16,7 @@
 package org.apache.geode.benchmark.tests;
 
 import static java.util.concurrent.TimeUnit.MINUTES;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 import org.apache.geode.perftest.TestConfig;
 
@@ -24,12 +25,12 @@ public class GeodeBenchmark {
   /**
    * Warm up time for the benchmark running on the default runner
    */
-  private static final long WARM_UP_TIME = MINUTES.toSeconds(1);
+  private static final long WARM_UP_TIME = SECONDS.toSeconds(30);
 
   /**
    * Total duration for which the benchmark will run on the default runner
    */
-  private static final long BENCHMARK_DURATION = MINUTES.toSeconds(5);
+  private static final long BENCHMARK_DURATION = MINUTES.toSeconds(1);
 
   /**
    * Number of threads to run benchmark.
