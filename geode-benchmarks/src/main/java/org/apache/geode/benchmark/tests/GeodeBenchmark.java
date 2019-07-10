dfe16471c60424c79f7ffb37aabe6e1738c11b41
Jacob Barrett <jbarrett@pivotal.io>
2019-07-09 22:52:11 -0700

    Revert "Faster runtime"
    This reverts commit d2f977e7

--- geode-benchmarks/src/main/java/org/apache/geode/benchmark/tests/GeodeBenchmark.java
+++ geode-benchmarks/src/main/java/org/apache/geode/benchmark/tests/GeodeBenchmark.java
@@ -25,12 +25,12 @@ public class GeodeBenchmark {
   /**
    * Warm up time for the benchmark running on the default runner
    */
-  private static final long WARM_UP_TIME = SECONDS.toSeconds(30);
+  private static final long WARM_UP_TIME = MINUTES.toSeconds(1);
 
   /**
    * Total duration for which the benchmark will run on the default runner
    */
-  private static final long BENCHMARK_DURATION = MINUTES.toSeconds(1);
+  private static final long BENCHMARK_DURATION = MINUTES.toSeconds(5);
 
   /**
    * Number of threads to run benchmark.
