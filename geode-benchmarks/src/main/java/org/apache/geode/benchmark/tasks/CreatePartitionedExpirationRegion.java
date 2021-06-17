19cd97cdd0fc7f840e082044144c81b78b15babc
Jacob Barrett <jbarrett@pivotal.io>
2021-06-17 16:33:11 -0700

    lru 1M

--- geode-benchmarks/src/main/java/org/apache/geode/benchmark/tasks/CreatePartitionedExpirationRegion.java
+++ geode-benchmarks/src/main/java/org/apache/geode/benchmark/tasks/CreatePartitionedExpirationRegion.java
@@ -35,8 +35,7 @@ public class CreatePartitionedExpirationRegion implements Task {
     final Cache cache = (Cache) context.getAttribute(SERVER_CACHE);
     cache.createRegionFactory(RegionShortcut.PARTITION_REDUNDANT)
         .setEntryIdleTimeout(new ExpirationAttributes(5, ExpirationAction.DESTROY))
-//        .setEntryTimeToLive(new ExpirationAttributes(5, ExpirationAction.DESTROY))
-        .setEvictionAttributes(EvictionAttributes.createLRUEntryAttributes(10000))
+        .setEvictionAttributes(EvictionAttributes.createLRUEntryAttributes(1_000_000))
         .create("region");
   }
 }
