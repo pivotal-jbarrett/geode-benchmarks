9f649ac2939d0fcab062d3ceba96586011e0811f
Jacob Barrett <jbarrett@pivotal.io>
2021-06-17 06:11:23 -0700

    Disable rebalance Disable restart

--- geode-benchmarks/src/main/java/org/apache/geode/benchmark/tests/PartitionedPutWithExpirationBenchmark.java
+++ geode-benchmarks/src/main/java/org/apache/geode/benchmark/tests/PartitionedPutWithExpirationBenchmark.java
@@ -55,10 +55,10 @@ public class PartitionedPutWithExpirationBenchmark extends AbstractPerformanceTe
 
     before(config, new CreatePartitionedExpirationRegion(), SERVER);
     before(config, new CreateClientProxyRegion(), CLIENT);
-    before(config, new RebalanceTask(), SERVER);
-    before(config,
-        new RestartServerTask(new StartServer(LOCATOR_PORT, EPHEMERAL_PORT), new StopServer(),
-            new CreatePartitionedExpirationRegion()), SERVER);
+//    before(config, new RebalanceTask(), SERVER);
+//    before(config,
+//        new RestartServerTask(new StartServer(LOCATOR_PORT, EPHEMERAL_PORT), new StopServer(),
+//            new CreatePartitionedExpirationRegion()), SERVER);
 
     workload(config, new PutRandomStringByteArrayTask(), CLIENT);
 
